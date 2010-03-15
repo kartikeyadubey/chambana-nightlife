823ae062ecf892375827fcc6998ec2fda69cc27f
Tom Patterson <tom@businesslogic.com>
2010-03-14 20:36:08 -0500

    removed local file
    Worked on by: pattertom

--- local.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost:8888";
-    $db_user = "root";
-    $db_pass = "root";
-   
-   // if no database is specified, set the default database
-   if ($db == "")
-       $db = "CS411_data";
-?>
